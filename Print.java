c12d24c3ff18bd464ac277fdabc9124a5d5920d1
Genarcarte <bang.bang.rabang@gmail.com>
2025-11-22 18:48:29 +0800

    feat: add Print.java with basic printing examples

---
+++ Print.java
@@ -0,0 +1,22 @@
+public class Print {
+
+        public static void main(String[] args) {
+        
+        System.out.print("I like ice cream!");
+        System.out.print("It's really good!");
+        System.out.print("Buy me ice cream!");
+        // without ln or \n it will print on the same line
+
+        System.out.println();
+        System.out.println("I like ice cream!");
+        System.out.print("It's really good!\n");
+        System.out.println("Buy me ice cream!");
+        // with ln or /n it will print on a new line
+
+        System.out.println();
+        /*  
+        type "sout" then press tab to print a new line 
+        */
+    }    
+    
+}
